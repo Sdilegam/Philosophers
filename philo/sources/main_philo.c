907e155e2c1ccdfeaf85669ebbfdfc7f43da02e9
sdi-lega <sdi-lega@student.s19.be>
2022-07-18 12:15:07 +0200

    Start and stumbling

--- philo/sources/main_philo.c
+++ philo/sources/main_philo.c
@@ -6,19 +6,39 @@
 /*   By: sdi-lega <sdi-lega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:54:50 by sdi-lega          #+#    #+#             */
-/*   Updated: 2022/07/18 10:22:16 by sdi-lega         ###   ########.fr       */
+/*   Updated: 2022/07/18 12:13:47 by sdi-lega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main_philo.h"
 #include <unistd.h>
+#include <stdio.h>
 #include <pthread.h>
+
+void *routine (void *number)
+{
+	int *test;
+	int index = -1;
+	test = (int *)number;
+	while (++index != 500000)
+	{
+		(*test)++;
+	}
+	return(test);
+}
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	test();
-	write(1,"Philo: test.\n", 13);
+	int test = 0;
+	pthread_t p1 = 0;
+	pthread_create(&p1, NULL, &routine, &test);
+	pthread_t p2 = 0;
+	pthread_create(&p2, NULL, &routine, &test);
+	pthread_join(p1, NULL);
+	printf("%d\n", test);
+	pthread_join(p2, NULL);
+	printf("%d\n", test);
 	return (0);
 }
